8a9b904108282b4d68e69bbaef2b3df25314c22f
Ben Torn <tornben@gmail.com>
2020-11-21 12:31:03 -0800

    added twilio code to chat app. In progress.

--- Chat_app/main.py
+++ Chat_app/main.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, join_room, leave_room
 
+# Added by Ben:
+import os
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
+
+# Add Twilio authentication
+account_sid = "ACcf38ea43fc81a1e3ad61701d6ebc096d"
+auth_token = "19de24461133f84dad89be010d3b2554"
+client = Client(account_sid, auth_token)
+
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 
@@ -13,10 +24,12 @@ def home():
 @app.route('/chat')
 def chat():
     username = request.args.get('username')
-    room = request.args.get('room')
+    # room = request.args.get('room')
+    room = 1
+    incoming_sms = request.args.get("incoming_sms")
 
     if username and room:
-        return render_template('chat.html', username=username, room=room)
+        return render_template('chat.html', username=username, room=room, incoming_sms=incoming_sms)
     else:
         return redirect(url_for('home'))
 
@@ -42,6 +55,43 @@ def handle_leave_room_event(data):
     leave_room(data['room'])
     socketio.emit('leave_room_announcement', data, room=data['room'])
 
+
+# Routes for Twilio API
+@app.route("/inbound_sms", methods=['GET', 'POST'])
+def inbound_sms():
+    response = MessagingResponse()
+
+    # Grab information from incoming message
+    inbound_message = request.form['Body']
+    from_number = request.form['From']
+    to_number = request.form['To']
+
+    # Print test messages
+    print(inbound_message)
+    print(from_number)
+    print(to_number)
+
+    # Resonse back to user
+    # response.message("Message recieved")
+
+    # Redirect to outbound_sms with message paramter
+    return redirect(url_for('outbound_sms', message=inbound_message))
+
+@app.route("/outbound_sms", methods=['GET', 'POST'])
+def outbound_sms():
+
+    # Capture message argument in a variable
+    incoming_sms = request.args.get("message")
+    print(incoming_sms) 
+
+    client.messages.create(
+    # to="+15148348842", #Khalid
+    to="+16045621572", #Ben
+    from_="+16479058445",
+    body=incoming_sms
+)
+ 
+    return redirect(url_for('chat', incoming_sms=incoming_sms, room=1))
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
